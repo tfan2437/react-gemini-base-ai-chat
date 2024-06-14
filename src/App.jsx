765dd3b1bac627a45dfb12fdbe3fe2a24cd0f3b5
tfan2437 <tfan2437@gmail.com>
2024-06-14 18:55:58 -0400

    Initial styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   );
 };
